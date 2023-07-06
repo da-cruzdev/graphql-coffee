0258321d39b0ec63839acfc785ca5332111180d0
Da-cruz <wilfrieddacruz@arolitec.com>
2023-07-06 17:33:54 +0000

    connecting to the database with typeorm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,10 +1,20 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { json } from 'stream/consumers';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType({ description: 'Coffee model' })
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'a unique identifier' })
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column({ type: 'json' })
   flavors: string[];
 }
