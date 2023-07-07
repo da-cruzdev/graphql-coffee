b8a0cc13901fafe2264e81956f20e2cf008f0019
Da-cruz <wilfrieddacruz@arolitec.com>
2023-07-07 12:35:25 +0000

    create union drink type

--- src/drinks/drinks.resolver.ts
+++ src/drinks/drinks.resolver.ts
@@ -1,12 +1,13 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from 'src/coffees/entities/coffee.entity';
 import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { DrinkResultUnion } from 'src/common/unions/drink-result.union';
 import { Tea } from 'src/teas/entities/tea.entity/tea.entity';
 
 @Resolver()
 export class DrinksResolver {
-  @Query(() => [Drink], { name: 'drinks' })
-  async findAll(): Promise<Drink[]> {
+  @Query(() => [DrinkResultUnion], { name: 'drinks' })
+  async findAll(): Promise<(typeof DrinkResultUnion)[]> {
     const coffee = new Coffee();
     coffee.id = 10;
     coffee.name = 'Colombia';
